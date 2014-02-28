edfb317043b94248dcd78ab3613279d7b28b3fda
Michele Facchetti <michele@duckma.com>
2014-02-28 11:47:23 +0100

    Initial Commit

---
+++ Truly Empty/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Truly Empty
+//
+//  Created by Michele Facchetti on 28/02/14.
+//  Copyright (c) 2014 Michele Facchetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Truly Empty/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Truly Empty
+//
+//  Created by Michele Facchetti on 28/02/14.
+//  Copyright (c) 2014 Michele Facchetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
