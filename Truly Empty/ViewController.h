67032f8856704e39b91780016585cd375da41d1a
Michele Facchetti <michele.facchetti@gmail.com>
2014-03-09 11:04:15 +0100

    Impostata la Base Internationalization.

---
+++ Truly Empty/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Truly Empty
+//
+//  Created by Michele Facchetti on 28/02/14.
+//  Copyright (c) 2014 Michele Facchetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
